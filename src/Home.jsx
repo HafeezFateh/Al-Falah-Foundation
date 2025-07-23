a3ce8b8915ded3410eea3a8899e5ef231bad8df7
Hafeezur Rahman <hafeezofficial08@gmail.com>
2025-07-23 14:35:05 +0530

    V.1.1.5

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,15 @@ function Home(){
     return (
 
         <>
-
         <Header />
+
+        <div id="hero" className="flex flex-col items-center mt-20 lg:mt-30 h-screen ">
+            
+            <div className="mx-5 flex flex-col justify-center ">
+            <h1 className="font-inter text-4xl lg:text-6xl font-semibold">We are here to spread the truth by the permisssion of ALLAH</h1>
+            </div>
+
+        </div>
            
         </>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,22 +8,35 @@ function Header(){
 
     return(
         <>
-            <div className="mx-3 mt-5 bg-gradient-to-br from-lime-500 via-lime-300 to-lime-500 p-[2px] rounded-xl shadow-lg">
-                <div className="bg-stone-100  rounded-xl shadow-lg p-4 flex justify-between ">
-                    <h1 className="text-lg font-semibold text-lime-900">Al Falah Foundation</h1>
-                    <span onClick={toggleMenu} class="material-symbols-outlined text-lime-700 ">menu</span>
+            <div className="bg-gradient-to-br from-lime-500 via-lime-300 to-lime-500 pb-[2px]  shadow-lg">
+                <div className="bg-stone-100   shadow-lg p-4 flex items-center justify-between ">
+                    <h1 className="text-xl font-semibold text-lime-900">Al Falah Foundation</h1>
+                    <span onClick={toggleMenu} class="material-symbols-outlined text-lime-700 "><span className="lg:hidden">menu</span></span>
+
+                    <div className="flex-grow text-lime-800 hidden lg:flex justify-center  gap-8 font-semibold text-lg">
+                        <Link to='/'>About</Link>
+                        <Link to='/'>Blog</Link>
+                        <Link to='/'>Materials</Link>
+                        <Link to='/'>Login</Link>
+                     </div>
+                    
+                    <div className="group hidden hover:shadow-lg hover:border-lime-500 lg:flex border border-lime-400 gap-3 items-center  rounded-xl px-8 py-1">
+                        <Link to='/'  className="text-lg font-semibold group-hover:text-lime-500">Join us</Link>
+                        <span class="material-symbols-outlined group-hover:translate-x-3 text-lime-700 transition">arrow_right_alt</span>
+
+                    </div>
                 </div>
-            </div>
+                </div>
 
 
             {open && 
-                <div className="mx-3 my-2 rounded-lg bg-gradient-to-br from-lime-500 via-lime-300 to-lime-500 p-[1px]">
-                    <div className="bg-stone-200 p-2 rounded-lg flex flex-col gap-2">
-                        <Link to='/about' className="w-full bg-stone-100/80 p-2 rounded-xl">About</Link>
-                        <Link to='/about' className="w-full bg-stone-100/80 p-2 rounded-xl">Blog</Link>
-                        <Link to='/about' className="w-full bg-stone-100/80 p-2 rounded-xl">Materials</Link>
-                        <Link to='/about' className="w-full bg-stone-100/80 p-2 rounded-xl">Login</Link>
-                        <hr className="text-stone-100 mt-2 "/>
+                <div className="bg-gradient-to-br from-lime-500 via-lime-300 to-lime-500 pb-[1px]">
+                    <div className="bg-lime-50 p-2  flex flex-col gap-2">
+                        <Link to='/about' className="w-full border border-lime-100 mt-2 bg-stone-50 p-2 rounded-xl">About</Link>
+                        <Link to='/about' className="w-full border border-lime-100 bg-stone-50 p-2 rounded-xl">Blog</Link>
+                        <Link to='/about' className="w-full border border-lime-100 bg-stone-50 p-2 rounded-xl">Materials</Link>
+                        <Link to='/about' className="w-full border border-lime-100 bg-stone-50 p-2 rounded-xl">Login</Link>
+                        <hr className="text-lime-300 mt-2 "/>
                         <div className="group p-2 flex gap-1">
                             <Link to='/about' className="font-semibold">Join</Link>
                             <span class="material-symbols-outlined group-hover:translate-x-3 text-lime-700 transition">chevron_right</span>
@@ -32,6 +45,8 @@ function Header(){
                     </div>
                 </div>
             }
+
+
         </>
     )
 }
